d696bd90244612b0faf09f1cab12664f61f7e079
burin <burin.ina@student.mahidol.edu>
2023-05-18 21:03:16 +0700

    Implemented deep q learning network

--- agent_linear.py
+++ agent_linear.py
@@ -102,30 +102,33 @@ def run_episode(for_training):
         None
     """
     epsilon = TRAINING_EP if for_training else TESTING_EP
-    epi_reward = None
+    epi_reward = 0
 
     # initialize for each episode
     # TODO Your code here
 
     (current_room_desc, current_quest_desc, terminal) = framework.newGame()
+    t = 0
     while not terminal:
         # Choose next action and execute
         current_state = current_room_desc + current_quest_desc
         current_state_vector = utils.extract_bow_feature_vector(
             current_state, dictionary)
-        # TODO Your code here
+        action_index, object_index = epsilon_greedy(current_state_vector, theta, epsilon)
+        next_room_desc, next_quest_desc, reward, terminal = framework.step_game(current_room_desc, current_quest_desc, action_index, object_index)
+        next_state = next_room_desc+next_quest_desc
+        next_state_vector = utils.extract_bow_feature_vector(next_state, dictionary)
 
         if for_training:
             # update Q-function.
-            # TODO Your code here
-            pass
+            linear_q_learning(theta, current_state_vector,action_index, object_index, reward, next_state_vector, terminal)
 
         if not for_training:
             # update reward
-            # TODO Your code here
-            pass
-
+            epi_reward += GAMMA **t * reward
+            t+=1
         # prepare next step
+        current_room_desc,current_quest_desc = next_room_desc, next_quest_desc
         # TODO Your code here
 
     if not for_training:
